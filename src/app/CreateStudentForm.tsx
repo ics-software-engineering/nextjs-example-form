61c53afa65b70eb38b2069d9f126291cf982e882
Cam Moore <cammymoore07@gmail.com>
2024-07-02 08:56:17 -1000

    Checkpoint
    CreateStudentForm, doesn't do anything yet.

--- src/app/CreateStudentForm.tsx
+++ src/app/CreateStudentForm.tsx
@@ -1,1 +1,123 @@
 "use client";
+
+import { useState } from "react";
+import { Hobby, Level, Major } from "@prisma/client";
+import { Form, Button, Col, Container, Card, Row } from "react-bootstrap";
+
+const CreateStudentForm = () => {
+  const formPadding = "py-1";
+  const levelKeys = Object.keys(Level).filter((key) => isNaN(Number(key)));
+  const hobbyKeys = Object.keys(Hobby).filter((key) => isNaN(Number(key)));
+  const majorKeys = Object.keys(Major).filter((key) => isNaN(Number(key)));
+  const gpaValues = ["0.0-0.9", "1.0-1.9", "2.0-2.9", "3.0-3.9", "4.0+"];
+
+  return (
+    <Container>
+      <Card>
+        <Card.Body>
+          <Form>
+            <Row className={formPadding}>
+              <Col>
+                <Form.Group controlId="formName">
+                  <Form.Label>Name</Form.Label>
+                  <Form.Control type="text" placeholder="Your name" />
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group controlId="formEmail">
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control type="email" placeholder="Your email" />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row className={formPadding}>
+              <Form.Group controlId="formBio">
+                <Form.Label>Biographical Statement</Form.Label>
+                <Form.Control as="textarea" placeholder="A bit about you" />
+              </Form.Group>
+            </Row>
+            <Row className={formPadding}>
+              <Col>
+                <Form.Group controlId="formLevel">
+                  <Form.Label>Level</Form.Label>
+                  <Form.Select>
+                    {levelKeys.map((level) => (
+                      <option key={level} value={level}>
+                        {level}
+                      </option>
+                    ))}
+                  </Form.Select>
+                  <Form.Text style={{ color: "red" }}>
+                    What is your level? (required)
+                  </Form.Text>
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group controlId="formGPA">
+                  <Form.Label>GPA</Form.Label>
+                  <Form.Select>
+                    {gpaValues.map((gpa, index) => (
+                      <option key={gpa} value={index}>
+                        {gpa}
+                      </option>
+                    ))}
+                  </Form.Select>
+                  <Form.Text style={{ color: "red" }}>
+                    Select one (required)
+                  </Form.Text>
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group controlId="formEnrolled">
+                  <Form.Label>Date Enrolled</Form.Label>
+                  <Form.Control type="date" />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row className={formPadding}>
+              <Form.Group controlId="formHobbies">
+                <Form.Label>Hobbies&nbsp;&nbsp;</Form.Label>
+                {hobbyKeys.map((hobby) => (
+                  <Form.Check
+                    key={hobby}
+                    inline
+                    type="checkbox"
+                    label={hobby}
+                    id={hobby}
+                  />
+                ))}
+                <Form.Text className="text-muted">
+                  {" "}
+                  Select hobbies (optional)
+                </Form.Text>
+              </Form.Group>
+            </Row>
+            <Row className={formPadding}>
+              <Form.Group controlId="formMajor">
+                <Form.Label>Major&nbsp;&nbsp;</Form.Label>
+                {majorKeys.map((major) => (
+                  <Form.Check
+                    key={major}
+                    inline
+                    type="radio"
+                    name="major"
+                    label={major}
+                    id={major}
+                  />
+                ))}
+                <Form.Text style={{ color: "red" }}>
+                  What is your major? (required)
+                </Form.Text>
+              </Form.Group>
+            </Row>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+    </Container>
+  );
+};
+
+export default CreateStudentForm;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import CreateStudentForm from "./CreateStudentForm";
+
 export default function Home() {
   return (
     <main>
       <h1 className="text-center">Create Student</h1>
+      <CreateStudentForm />
     </main>
   );
 }
