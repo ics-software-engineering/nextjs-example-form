93c171297ceacb0ed5d5d8fbc0371e647a4b4e3c
Cam Moore <cammymoore07@gmail.com>
2024-07-02 07:26:21 -1000

    Update dbActions.ts
    Added upsertStudent. This can be used to create and edit student information.

--- src/lib/dbActions.ts
+++ src/lib/dbActions.ts
@@ -3,14 +3,18 @@
 import { StudentData, EnrollmentData } from "@prisma/client";
 import { prisma } from "@/lib/prisma";
 
-const createStudent = async (
+const upsertStudent = async (
   studentData: StudentData,
   enrollmentData: EnrollmentData,
 ) => {
-  // CAM: should we check if the student already exists? Or just upsert?
   const student = await prisma.studentData.upsert({
     where: { email: studentData.email },
     update: studentData,
     create: studentData,
+  });
+  const enrollment = await prisma.enrollmentData.upsert({
+    where: { email: enrollmentData.email },
+    update: enrollmentData,
+    create: enrollmentData,
   });
 };
