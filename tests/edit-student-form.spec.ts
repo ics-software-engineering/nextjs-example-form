2c5e4687a49707b8104db2f97d4c1686419be278
Cam Moore <cammymoore07@gmail.com>
2024-07-07 10:57:37 -1000

    Update edit-student-form.spec.ts

--- tests/edit-student-form.spec.ts
+++ tests/edit-student-form.spec.ts
@@ -1,6 +1,8 @@
 import { test, expect } from '@playwright/test';
 
 test('Edit Student Form', async ({ page }) => {
+  // This test requires the student cmoore@foo.com to exist in the database.
+  // Create Student Form test can be used to create this student.
   await page.goto('https://localhost:3000/student/cmoore@foo.com');
   await expect(page.getByRole('heading', { name: 'Edit Student' })).toBeVisible();
   await expect(page.getByText('Major*')).toBeVisible();
